4100317c91e5be2d5682a1ad7d29dbb7da9b2c05
Jose Hernandez <josedhg29@gmail.com>
2022-07-20 00:45:32 -0400

    Fixed express fuckup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require('cors')
+
 const app = express();
 
 // parse requests of content-type: application/json
